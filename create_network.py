d260a5c886e36491c4eda5281eb52bd1db152d1c
Yuxin Chen <chenyuxincyx048@gmail.com>
2023-08-18 18:29:23 +0800

    cyx

--- create_network.py
+++ create_network.py
@@ -25,17 +25,17 @@ def generate_random_nodes(num_nodes):
              "圆某人和四某人","英超联赛","不能吃甜辣","张路评球","Wink0v0","AKUM","早安没吃饱","AkiXXXL","曼城足球俱乐部","默离"]
     
     '''
-    
+    '''
     users = ['体坛周报','萌宠百科','黄健翔','Rukia_wjy','周琦_26','zhuluojiong','马小良','冯潇','溜溜哥','赛车星冰乐',
             '我叫蓝姐','车手叶一飞','克总评车','狂飙客','韩寒','大油门的548','Superryry','Tommy12224','许子东','李武军',
             '曹亚旗','帅车评','巢巢巢巢巢Even','董路','李方妮','方正宇','崔岳2022','车手周冠宇','刘耀_FLy','老虎受欢迎',
             '疯狂历史','刘叔Joe','新水令','小灰羊是杨晓辉','庄宁宇','上海申花足球俱乐部','娄一晨','月上寻兔呀','羽毛球杂志','Kiya']
     '''
     users = ['植物眼','内含子','河森堡','折翼丛林','贼叉','天冬','ZKColorful','常岩CY','胡正阳','硬哥',
-            '植物眼1','内含子1','河森堡1','折翼丛林1','贼叉1','天冬1','ZKColorful1','常岩CY1','胡正阳1','硬哥1',
-            '植物眼2','内含子2','河森堡3','折翼丛林4','贼叉5','天冬6','ZKColorful8','常岩CY11','胡正阳12','硬哥13',
-            '植物眼21','内含子21','河森堡13','折翼丛林41','贼叉51','天冬61','ZKColorful81','常岩CY111','胡正阳112','硬哥113']
-    '''
+            '三思逍遥','航空新视野-赤卫','前站起飞','民航吐槽君','了不起的中国制造','植物人史军','LovePlane','地球知识局','微博航空','云无心45',
+            '中科院高能所','疾风风','欢乐的云端之上','ZHCH21','草根航评','ZKColorful8','常岩CY11','胡永平','小特叔叔',
+            '亢岳','铜片','PS3保罗','韩璐','老七','42号车库','花叔1974','pony龙','杰克涛','沉的乐','李老鼠说车']
+    
     for i in range(num_nodes):  
         x, y = get_random_in_range(-1500, 1500), get_random_in_range(-1000, 1000)
         nodes.append({  
@@ -51,13 +51,13 @@ def generate_random_nodes(num_nodes):
         {
             "color": get_random_color(),  
             #"label": "余霜YSCandice",  
-            "label": "我是F1车手兵哥",
-            #"label":"空中的士马宏",
+            #"label": "我是F1车手兵哥",
+            "label":"空中的士马宏",
             "attributes": {"关系":"本人"},  
             "y": 0,  
             "x": 0,  
-            "id": "我是F1车手兵哥",
-            #"id": "空中的士马宏",  
+            #"id": "我是F1车手兵哥",
+            "id": "空中的士马宏",  
             #"id": "余霜YSCandice",  
             "size": 60 
         }
@@ -71,7 +71,7 @@ def generate_random_edges(num_edges):
              "Ale-胡嘉乐","解说瓜瓜","石头赶路ing","管泽元","彭立勋","LNG-scout","白宇WHITE","ssenming","我是郭杰瑞","ikussuki",
              "解说王淞","解说Kinko人","朱一龙","小米粥","年糕糕呀","陈欢","新浪游戏","解说Zonic","雨说体育徐静雨","楼盘黑衣人","YeGodzz",
              "圆某人和四某人","英超联赛","不能吃甜辣","张路评球","Wink0v0","AKUM","早安没吃饱","AkiXXXL","曼城足球俱乐部","默离"]
-    '''
+    
     
     users = ['体坛周报','萌宠百科','黄健翔','Rukia_wjy','周琦_26','zhuluojiong','马小良','冯潇','溜溜哥','赛车星冰乐',
             '我叫蓝姐','车手叶一飞','克总评车','狂飙客','韩寒','大油门的548','Superryry','Tommy12224','许子东','李武军',
@@ -81,15 +81,16 @@ def generate_random_edges(num_edges):
     '''
 
     users = ['植物眼','内含子','河森堡','折翼丛林','贼叉','天冬','ZKColorful','常岩CY','胡正阳','硬哥',
-            '植物眼1','内含子1','河森堡1','折翼丛林1','贼叉1','天冬1','ZKColorful1','常岩CY1','胡正阳1','硬哥1',
-            '植物眼2','内含子2','河森堡3','折翼丛林4','贼叉5','天冬6','ZKColorful8','常岩CY11','胡正阳12','硬哥13',
-            '植物眼21','内含子21','河森堡13','折翼丛林41','贼叉51','天冬61','ZKColorful81','常岩CY111','胡正阳112','硬哥113']
-    '''
+        '三思逍遥','航空新视野-赤卫','前站起飞','民航吐槽君','了不起的中国制造','植物人史军','LovePlane','地球知识局','微博航空','云无心45',
+        '中科院高能所','疾风风','欢乐的云端之上','ZHCH21','草根航评','ZKColorful8','常岩CY11','胡永平','小特叔叔',
+        '亢岳','铜片','PS3保罗','韩璐','老七','42号车库','花叔1974','pony龙','杰克涛','沉的乐','李老鼠说车']
+    
+    
     for i in range(num_nodes):
         edges.append({
             #"sourceID": "余霜YSCandice",
-            "sourceID": "我是F1车手兵哥",
-            #"sourceID": "空中的士马宏",
+            #"sourceID": "我是F1车手兵哥",
+            "sourceID": "空中的士马宏",
             "attributes":{},
             "targetID": users[i],
             "size": 1
@@ -121,12 +122,12 @@ def generate_random_edges(num_edges):
     return edges  
   
 num_nodes = 40 #其他节点个数  
-num_edges = 50  
+num_edges = 35  
   
 random_data = {  
     "nodes": generate_random_nodes(num_nodes),  
     "edges": generate_random_edges(num_edges)  
 }  
   
-with open('layui/我是F1兵哥_network.json','w', encoding='utf-8') as file:
+with open('layui/空中的士马宏_network.json','w', encoding='utf-8') as file:
     json.dump(random_data, file, indent=2, ensure_ascii=False)
